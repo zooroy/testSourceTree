3c2dc9891e13ea0a7046f8eebe7bcdd3e5a7b679
Roy Hsiao <zooroy13@gmail.com>
2019-10-16 22:18:54 +0800

    test hello 2

--- src/components/Hello.js
+++ src/components/Hello.js
@@ -4,7 +4,7 @@ class Hello extends React.Component {
   render() {
     return (
       <div>
-        <h1>{this.props.text}</h1>;
+        <h1>{this.props.text}</h1>;<h2>123</h2>
       </div>
     );
   }
